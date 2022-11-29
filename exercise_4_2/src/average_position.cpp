40e27ad4577c9c44d87c85c2ad039f57c956c7c6
jemoncadar <jemonra@uma.es>
2022-11-29 19:06:37 +0100

    added links with info about images in ROS and OpenCV

--- exercise_4_2/src/average_position.cpp
+++ exercise_4_2/src/average_position.cpp
@@ -1,8 +1,9 @@
-#include "apriltag_ros/AprilTagDetectionArray.h"
-#include "ros/ros.h"
+#include <apriltag_ros/AprilTagDetectionArray.h>
+#include <ros/ros.h>
 #include <opencv2/opencv.hpp>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <image_transport/image_transport.h>
 
 // Global variables to manage average
 double x_sum;
@@ -19,6 +20,8 @@ bool first_detection = true;
 
 std::vector<double> cameraK;
 std::vector<double> cameraD;
+
+std::vector<unsigned int> outputImage;
 
 void chatterCallbackTagDetections(const apriltag_ros::AprilTagDetectionArray& msg)
 {
@@ -48,16 +51,21 @@ void chatterCallbackTagDetections(const apriltag_ros::AprilTagDetectionArray& ms
 	}
 }
 
-void chatterCallbackImages(const sensor_msgs::Image& img)
+void chatterCallbackImages(const sensor_msgs::ImageConstPtr& msg)
 {
+    ROS_INFO("Got image!");
+
     const auto& rvec = (cv::Mat_<float>(3,1) << 0.0, 0.0, 0.0);
     const auto& tvec = (cv::Mat_<float>(3,1) << x_mean, y_mean, z_mean);
 
-    //cv::projectPoints( InputArray objectPoints,InputArray rvec, InputArray tvec, InputArray cameraMatrix, InputArray distCoeffs, 
-    //OutputArray imagePoints, OutputArray jacobian=noArray(), double aspectRatio=0);
-
-    // Necesitamos "cameraMatrix" y "distCoeffs" que se encuentran en el topic "/kinect/rgb/camera_info". Debemos suscribirnos
-    // a dicho topic también
+    /*cv_bridge::CvImagePtr cvImagePtr;
+    try {
+        cvImagePtr = cv_bridge::toCvCopy(msg);
+    } catch (cv_bridge::Exception &e) {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+    }*/
+    //https://answers.ros.org/question/241499/cmakeliststxt-and-packagexml-files-of-ros-image-to-cv-image-conversion/
+    //http://www.cs.cmu.edu/~cga/xxx/notes-images.pdf
 }
 
 int main(int argc, char **argv)
@@ -67,8 +75,11 @@ int main(int argc, char **argv)
 	ROS_INFO("Node started");
 
 	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("tag_detections",1000, chatterCallbackTagDetections);
-    ros::Subscriber subImages = n.subscribe("kinect/rgb/image_rect/color",1000, chatterCallbackImages);
+	ros::Subscriber subTags = n.subscribe("tag_detections",1000, chatterCallbackTagDetections);
+
+    //ros::Subscriber subImages = n.subscribe("kinect/rgb/image_rect/color",1000, chatterCallbackImages);
+    image_transport::ImageTransport it(n);
+    image_transport::Subscriber subImages = it.subscribe("kinect/rgb/image_rect_color", 1000, chatterCallbackImages);
 	
     //Receiving camera info
     boost::shared_ptr<sensor_msgs::CameraInfo const> spCameraInfo;
@@ -78,12 +89,17 @@ int main(int argc, char **argv)
         cameraInfo = *spCameraInfo;
     }
     ROS_INFO("Got camera info!");
-    //cameraK = cameraInfo.K;
-    //for (double d : cameraK)
-    //{
-    //    ROS_INFO("%f", d);
-    //}
-    //cameraD = cameraInfo.D;
+    const auto& cameraInfoD = cameraInfo.D;
+    for (double e : cameraInfoD)
+    {
+        cameraD.push_back(e);
+    }
+
+    const auto& cameraInfoK = cameraInfo.K;
+    for (double e : cameraInfoK)
+    {
+        cameraK.push_back(e);
+    }
 
 	ros::spin();
 	
