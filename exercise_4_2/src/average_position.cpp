7623474af7cb48b33f3db99c5a54247530627a15
jemoncadar <jemonra@uma.es>
2022-11-30 12:59:49 +0100

    output video working, todo: draw average point

--- exercise_4_2/src/average_position.cpp
+++ exercise_4_2/src/average_position.cpp
@@ -1,8 +1,13 @@
-#include <apriltag_ros/AprilTagDetectionArray.h>
 #include <ros/ros.h>
-#include <opencv2/opencv.hpp>
+
+#include <apriltag_ros/AprilTagDetectionArray.h>
+
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
+
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+
 #include <image_transport/image_transport.h>
 
 // Global variables to manage average
@@ -21,7 +26,8 @@ bool first_detection = true;
 std::vector<double> cameraK;
 std::vector<double> cameraD;
 
-std::vector<unsigned int> outputImage;
+// Global variables to manage images
+cv_bridge::CvImagePtr cv_ptr;
 
 void chatterCallbackTagDetections(const apriltag_ros::AprilTagDetectionArray& msg)
 {
@@ -55,15 +61,24 @@ void chatterCallbackImages(const sensor_msgs::ImageConstPtr& msg)
 {
     ROS_INFO("Got image!");
 
-    const auto& rvec = (cv::Mat_<float>(3,1) << 0.0, 0.0, 0.0);
-    const auto& tvec = (cv::Mat_<float>(3,1) << x_mean, y_mean, z_mean);
+    //const auto& rvec = (cv::Mat_<float>(3,1) << 0.0, 0.0, 0.0);
+    //const auto& tvec = (cv::Mat_<float>(3,1) << x_mean, y_mean, z_mean);
 
-    /*cv_bridge::CvImagePtr cvImagePtr;
-    try {
-        cvImagePtr = cv_bridge::toCvCopy(msg);
-    } catch (cv_bridge::Exception &e) {
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
         ROS_ERROR("cv_bridge exception: %s", e.what());
-    }*/
+        return;
+    }
+
+    // This is creating a circle in the image
+    // TODO: print average
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      cv::circle(cv_ptr->image, cv::Point(150, 150), 1000, CV_RGB(255,0,0));
+
     //https://answers.ros.org/question/241499/cmakeliststxt-and-packagexml-files-of-ros-image-to-cv-image-conversion/
     //http://www.cs.cmu.edu/~cga/xxx/notes-images.pdf
 }
@@ -80,7 +95,9 @@ int main(int argc, char **argv)
     //ros::Subscriber subImages = n.subscribe("kinect/rgb/image_rect/color",1000, chatterCallbackImages);
     image_transport::ImageTransport it(n);
     image_transport::Subscriber subImages = it.subscribe("kinect/rgb/image_rect_color", 1000, chatterCallbackImages);
-	
+
+    image_transport::Publisher pubImages = it.advertise("average_position", 1);
+
     //Receiving camera info
     boost::shared_ptr<sensor_msgs::CameraInfo const> spCameraInfo;
     sensor_msgs::CameraInfo cameraInfo;
@@ -101,7 +118,15 @@ int main(int argc, char **argv)
         cameraK.push_back(e);
     }
 
-	ros::spin();
+    ros::Rate rate(10); // 10 fps our video output
+    while(ros::ok())
+    {
+        if (cv_ptr != NULL)
+            pubImages.publish(cv_ptr->toImageMsg());
+        ros::spinOnce();
+
+        rate.sleep();
+    }
 	
 	return 0;
 }
